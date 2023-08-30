3010491c50aa30e29fe63561b3090cbd0a0dfb82
kartikver15gr8 <vermakartikey786@gmail.com>
2023-08-31 02:38:29 +0530

    added carousel

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -1,8 +1,55 @@
+import { useState } from "react";
 import Navbar from "./Navbar";
 import { useNavigate } from "react-router-dom";
 
 function Landing() {
-  return <></>;
+  const [carousel, setCarousel] = useState("../public/carousel3.jpg");
+  const [count, setCount] = useState(2);
+
+  return (
+    <div
+      className="container"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        // height: "35vh",
+      }}
+    >
+      <img
+        src={carousel}
+        alt=""
+        style={{ width: "100vw", height: "50vh", zIndex: "-1" }}
+      />
+      <button
+        style={{ zIndex: "1" }}
+        onClick={() => {
+          if (count > 2) {
+            setCount(count - 1);
+            setCarousel(`carousel${count}.jpg`);
+          } else {
+            setCount(6);
+            setCarousel(`carousel${count}.jpg`);
+          }
+        }}
+      >
+        prev
+      </button>
+      <button
+        style={{ zIndex: "1" }}
+        onClick={() => {
+          if (count < 6) {
+            setCount(count + 1);
+            setCarousel(`carousel${count}.jpg`);
+          } else {
+            setCount(2);
+            setCarousel(`carousel${count}.jpg`);
+          }
+        }}
+      >
+        next
+      </button>
+    </div>
+  );
 }
 
 export default Landing;
