fd29c464bcb98eea4f54473eabbd367d4057fc17
kartikver15gr8 <vermakartikey786@gmail.com>
2023-10-06 00:09:39 +0530

    modified App.tsx

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,5 +1,10 @@
+import { RecoilRoot, useSetRecoilState } from "recoil"
+import { userState } from "./store/atoms/user.ts"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import useNavigate from 'react-use-navigate';
+import { useEffect } from "react";
+import axios from 'axios'
+import { BASE_URL } from "./config.tsx"
+
 import Navbar from "./components/Navbar"
 import Landing from "./components/Landing";
 import Signup from "./components/Signup";
@@ -7,23 +12,60 @@ import Login from "./components/Login";
 
 import './App.css'
 
-
-
 function App() {
 
   return (
-    <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Landing />}></Route>
-          <Route path="/signup" element={<Signup />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-        </Routes>
-      </Router>
-
-    </>
+    <RecoilRoot>
+      <div
+        style={{ width: "100vw", height: "100vh", backgroundColor: "#eeeeee" }}
+      >
+        <Router>
+          <Navbar />
+          <InitUser />
+          <Routes>
+            <Route path="/" element={<Landing />}></Route>
+            <Route path="/signup" element={<Signup />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+          </Routes>
+        </Router>
+      </div>
+    </RecoilRoot>
   )
 }
 
-export default App
+
+function InitUser() {
+  const setUser = useSetRecoilState(userState);
+  const init = async () => {
+    try {
+      const response = await axios.get(`${BASE_URL}/user/me`, {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      });
+
+      if (response.data.email) {
+        setUser({
+          isLoading: false,
+          userEmail: response.data.email,
+        });
+      } else {
+        setUser({
+          isLoading: false,
+          userEmail: null,
+        });
+      }
+    } catch (e) {
+      setUser({
+        isLoading: false,
+        userEmail: null,
+      });
+    }
+  };
+  useEffect(() => {
+    init();
+  }, []);
+
+  return <></>;
+}
+export default App;
