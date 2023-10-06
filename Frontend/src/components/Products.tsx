c4bd90842d50516da2795c8cf62ba0db1e0ea3d3
kartikver15gr8 <vermakartikey786@gmail.com>
2023-10-06 20:04:15 +0530

    Added Products Component

---
+++ Frontend/src/components/Products.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import { Button, Card, Typography } from "@mui/material";
+import { BASE_URL } from '../config'
+// import { useNavigate } from 'react-router-dom'
+import axios from 'axios';
+
+
+export default function Products() {
+    const [products, setProducts] = useState([])
+    const init = async () => {
+        const response = await axios.get(`${BASE_URL}/admin/products/`, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+        });
+        console.log(response.data.products)
+        setProducts(response.data);
+    };
+
+    useEffect(() => {
+        init();
+    }, []);
+
+    return (
+        <div
+            style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
+        >
+            {products.map((products) => {
+                return <Product product={products} />;
+            })}
+        </div>
+    );
+};
+
+
+export function Product({ product }) {
+
+
+    return (
+        <Card
+            style={{
+                margin: 10,
+                width: 300,
+                minHeight: 200,
+                padding: 20,
+            }}
+        >
+            <Typography textAlign={"center"} variant="h5">
+                {product.title}
+            </Typography>
+            <Typography textAlign={"center"} variant="subtitle1">
+                {product.description}
+            </Typography>
+            <img src={product.imgLink} style={{ width: 300 }}></img>
+            <div style={{ display: "flex", justifyContent: "center", marginTop: 20 }}>
+
+                <Button
+                    variant="contained"
+                    size="large"
+
+                >
+                    Buy Price: {product.price}
+                </Button>
+            </div>
+        </Card>
+    );
+}
