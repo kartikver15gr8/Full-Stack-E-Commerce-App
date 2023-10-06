970d22eb27c53c7598caf34b7ee893f517ceb535
kartikver15gr8 <vermakartikey786@gmail.com>
2023-10-07 01:18:49 +0530

    Modified Landing.tsx component

--- Frontend/src/components/Landing.tsx
+++ Frontend/src/components/Landing.tsx
@@ -2,100 +2,104 @@ import { useState } from "react";
 // import Navbar from "./Navbar";
 // import { useNavigate } from "react-router-dom";
 import "./styles/landing.css";
-import Products from './Products'
+import Products from './Products';
+import Footer from "./Footer";
 
 function Landing() {
     const [carousel, setCarousel] = useState("../public/carousel3.jpg");
     const [count, setCount] = useState(2);
 
     return (
-        <div
-            className="mainBox"
-            style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                flexDirection: "column",
-            }}
-        >
+        <>
             <div
-                className="container"
+                className="mainBox"
                 style={{
                     display: "flex",
                     justifyContent: "center",
-                    flexDirection: "row",
-                    // height: "35vh",
+                    alignItems: "center",
+                    flexDirection: "column",
                 }}
             >
                 <div
-                    className="imageContainer"
+                    className="container"
                     style={{
-                        display: "inline-block",
-                        position: "relative",
-                        overflow: "hidden",
+                        display: "flex",
+                        justifyContent: "center",
+                        flexDirection: "row",
+                        // height: "35vh",
                     }}
                 >
-                    <button
-                        className="overBtn"
+                    <div
+                        className="imageContainer"
                         style={{
-                            position: "absolute",
-                            top: 0,
-                            left: 0,
-                            backgroundColor: "rgba(0, 0, 0, 0.3)",
-                            zIndex: "1",
-                            border: "none",
-                            height: "100%",
-                            width: "4%",
-                        }}
-                        onClick={() => {
-                            if (count > 2) {
-                                setCount(count - 1);
-                                setCarousel(`carousel${count}.jpg`);
-                            } else {
-                                setCount(6);
-                                setCarousel(`carousel${count}.jpg`);
-                            }
+                            display: "inline-block",
+                            position: "relative",
+                            overflow: "hidden",
                         }}
                     >
-                        ◀
-                    </button>
+                        <button
+                            className="overBtn"
+                            style={{
+                                position: "absolute",
+                                top: 0,
+                                left: 0,
+                                backgroundColor: "rgba(0, 0, 0, 0.3)",
+                                zIndex: "1",
+                                border: "none",
+                                height: "100%",
+                                width: "4%",
+                            }}
+                            onClick={() => {
+                                if (count > 2) {
+                                    setCount(count - 1);
+                                    setCarousel(`carousel${count}.jpg`);
+                                } else {
+                                    setCount(6);
+                                    setCarousel(`carousel${count}.jpg`);
+                                }
+                            }}
+                        >
+                            ◀
+                        </button>
 
-                    <img
-                        className="img"
-                        src={carousel}
-                        alt=""
-                        style={{
-                            width: "95vw",
-                        }}
-                    />
-                    <button
-                        className="overBtn"
-                        style={{
-                            position: "absolute",
-                            top: 0,
-                            right: 0,
-                            backgroundColor: "rgba(0, 0, 0, 0.3)",
-                            zIndex: "1",
-                            border: "none",
-                            height: "100%",
-                            width: "4%",
-                        }}
-                        onClick={() => {
-                            if (count < 6) {
-                                setCount(count + 1);
-                                setCarousel(`carousel${count}.jpg`);
-                            } else {
-                                setCount(2);
-                                setCarousel(`carousel${count}.jpg`);
-                            }
-                        }}
-                    >
-                        ▶
-                    </button>
+                        <img
+                            className="img"
+                            src={carousel}
+                            alt=""
+                            style={{
+                                width: "95vw",
+                            }}
+                        />
+                        <button
+                            className="overBtn"
+                            style={{
+                                position: "absolute",
+                                top: 0,
+                                right: 0,
+                                backgroundColor: "rgba(0, 0, 0, 0.3)",
+                                zIndex: "1",
+                                border: "none",
+                                height: "100%",
+                                width: "4%",
+                            }}
+                            onClick={() => {
+                                if (count < 6) {
+                                    setCount(count + 1);
+                                    setCarousel(`carousel${count}.jpg`);
+                                } else {
+                                    setCount(2);
+                                    setCarousel(`carousel${count}.jpg`);
+                                }
+                            }}
+                        >
+                            ▶
+                        </button>
+                    </div>
                 </div>
+                <Products />
             </div>
-            <Products />
-        </div>
+            <Footer />
+        </>
     );
 }
 
