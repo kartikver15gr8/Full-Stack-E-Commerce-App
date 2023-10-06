bc771cea3f49488c205ab3bd523bf2fcd3dac117
kartikver15gr8 <vermakartikey786@gmail.com>
2023-10-06 19:26:45 +0530

    modified Navbar component

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -1,53 +1,124 @@
 import "./styles/navbar.css";
 import { useNavigate } from "react-router-dom";
+import { useSetRecoilState, useRecoilValue } from "recoil";
+import { userState } from "../store/atoms/user";
+import { userEmailState } from "../store/selectors/userEmail"
 // import React from "react";
 
 function Navbar() {
     const navigate = useNavigate();
-    return (
-        <div className="navdiv">
-            <nav
-                className="navbar"
-                style={{ backgroundColor: "rgb(19, 25, 33)", paddingTop: "15px" }}
-            >
-                <ul>
-                    <img
-                        src="../public/ama.png"
-                        alt="Amazon"
-                        style={{ width: "100px" }}
-                        onClick={() => {
-                            navigate("/");
-                        }}
-                    />
-                    <input
-                        type="text"
-                        name="search"
-                        placeholder="Search"
-                        className="searchBox"
-                    />
-                    <button className="searchBtn">
-                        <img src="../public/search.png" alt="" />
-                    </button>
-                    <button
-                        className="signup"
-                        onClick={() => {
-                            navigate("/signup");
-                        }}
-                    >
-                        signup
-                    </button>
-                    <button
-                        className="login"
-                        onClick={() => {
-                            navigate("/login");
-                        }}
-                    >
-                        login
-                    </button>
-                </ul>
-            </nav>
-        </div>
-    );
+
+    const userEmail = useRecoilValue(userEmailState)
+    const setUser = useSetRecoilState(userState)
+
+    if (userEmail) {
+        return (
+            <div className="navdiv">
+                <nav
+                    className="navbar"
+                    style={{ backgroundColor: "rgb(19, 25, 33)", paddingTop: "15px" }}
+                >
+                    <ul>
+                        <img
+                            src="../public/ama.png"
+                            alt="Amazon"
+                            style={{ width: "100px" }}
+                            onClick={() => {
+                                navigate("/");
+                            }}
+                        />
+                        <input
+                            type="text"
+                            name="search"
+                            placeholder="Search"
+                            className="searchBox"
+                        />
+                        <button className="searchBtn">
+                            <img src="../public/search.png" alt="" />
+                        </button>
+                        {/* <button
+                            className="signup"
+                            onClick={() => {
+                                navigate("/signup");
+                            }}
+                        >
+                            signup
+                        </button>
+                        <button
+                            className="login"
+                            onClick={() => {
+                                navigate("/login");
+                            }}
+                        >
+                            login
+                        </button> */}
+                        <div className="user">
+                            <p style={{ color: 'whitesmoke' }}>{userEmail}</p>
+                        </div>
+                        <button
+                            className="logout"
+                            onClick={() => {
+                                setUser({
+                                    isLoading: false,
+                                    userEmail: null
+                                })
+                                navigate('signup')
+                            }}
+                        >Logout</button>
+                    </ul>
+                </nav>
+            </div>
+        )
+    }
+
+    else {
+
+
+        return (
+            <div className="navdiv">
+                <nav
+                    className="navbar"
+                    style={{ backgroundColor: "rgb(19, 25, 33)", paddingTop: "15px" }}
+                >
+                    <ul>
+                        <img
+                            src="../public/ama.png"
+                            alt="Amazon"
+                            style={{ width: "100px" }}
+                            onClick={() => {
+                                navigate("/");
+                            }}
+                        />
+                        <input
+                            type="text"
+                            name="search"
+                            placeholder="Search"
+                            className="searchBox"
+                        />
+                        <button className="searchBtn">
+                            <img src="../public/search.png" alt="" />
+                        </button>
+                        <button
+                            className="signup"
+                            onClick={() => {
+                                navigate("/signup");
+                            }}
+                        >
+                            signup
+                        </button>
+                        <button
+                            className="login"
+                            onClick={() => {
+                                navigate("/login");
+                            }}
+                        >
+                            login
+                        </button>
+                    </ul>
+                </nav>
+            </div>
+        );
+    }
 }
 
 export default Navbar;
