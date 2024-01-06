474151f677cf22fbb635ce6ea27335cb7bf387dc
kartikver15gr8 <vermakartikey786@gmail.com>
2024-01-07 00:55:46 +0530

    Made a ProductDetail Componenet

--- Frontend/src/components/ProductDetail.jsx
+++ Frontend/src/components/ProductDetail.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./Navbar";
+import { productState } from "../store/atoms/product";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 
 export default function ProductDetail() {
+  const pro = useRecoilValue(productState);
+  const [product, setProduct] = useState(pro);
+
+  console.log(product);
   return (
     <div className="container">
       <h1 style={{ color: "black", marginTop: "100px" }}>Product</h1>
+      <h2>{product.productDetails._id}</h2>
+      <h2>{product.productDetails.title}</h2>
+      <h2>{product.productDetails.description}</h2>
+      <h2>{product.productDetails.price}</h2>
+      <img src={product.productDetails.imgLink} alt="" />
     </div>
   );
 }
