9f1aaecd4b7b9686cc8395722330a9aa2928e540
kartikver15gr8 <vermakartikey786@gmail.com>
2024-01-17 01:37:17 +0530

    checkpoint

---
+++ Frontend/src/store/selectors/getProduct.tsx
@@ -0,0 +1,64 @@
+import { productState } from "../atoms/product";
+import { selector } from "recoil";
+
+export const isProductLoading = selector({
+    key: 'isProductLoading',
+    get: ({ get }) => {
+        const state = get(productState);
+
+        return state.isLoading;
+    },
+});
+
+export const productDetails = selector({
+    key: 'productDetailsState',
+    get: ({ get }) => {
+        const state = get(productState);
+
+        return state.productDetails;
+    },
+});
+
+export const productDetailsTitle = selector({
+    key: 'productTitleState',
+    get: ({ get }) => {
+        const state = get(productState);
+        if (state.productDetails) {
+            return state.productDetails.title;
+        }
+        return "";
+    },
+});
+
+export const productDetailsDescription = selector({
+    key: 'productDescriptionState',
+    get: ({ get }) => {
+        const state = get(productState);
+        if (state.productDetails) {
+            return state.productDetails.description;
+        }
+        return "";
+    },
+});
+
+export const productDetailsPrice = selector({
+    key: 'productDetailsPriceState',
+    get: ({ get }) => {
+        const state = get(productState);
+        if (state.productDetails) {
+            return state.productDetails.price;
+        }
+        return "";
+    },
+});
+
+export const productDetailsImage = selector({
+    key: 'productDetailsImageState',
+    get: ({ get }) => {
+        const state = get(productState);
+        if (state.productDetails) {
+            return state.productDetails.imgLink;
+        }
+        return "";
+    },
+});
